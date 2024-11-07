55f48aeb4b5161d7b6f414f9c41fffc83f7245af
JUAN GABRIEL ALMEIDA BENJAMIM <JUAN6105@digitalcollegesul.local>
2024-11-06 22:03:49 -0300

    feat: add json server

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,64 @@
+import { useEffect, useState } from "react";
 import { CardAside } from "./components/CardAside";
 import { Post } from "./components/Post";
 import { Title } from "./components/Title";
 
+export type User = {
+id: number;
+name: string;
+role: string;
+profileURL: string;
+}
 
+export type comment = {
+  userId: number;
+  content: string;
+  publishedAt: string;
+  likes: number;
+}
+
+export type Post = {
+  id: number;
+  userId: number;
+  content: string;
+  hashtags: string[];
+  comments: {
+    
+      userId: number,
+      content: string,
+      publishedAt: string,
+      likes: number,
+    
+  }[]
+}
 
 export default function App() {
+  const [posts, setPosts] = useState<Post[]>([]) 
+
+  async function getPosts() {
+    try {
+      const response = await fetch('http://localhost:3000/posts')
+      const data: Post[] = await response.json()
+      setPosts(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
     <>
-     <Title/>
-     <div className="flex justify-center gap-8 items-start py-8 bg-zinc-900 h-screen w-screen">
-     <CardAside/>
-     <Post/>
-     </div>
+      <Title />
+      <div className="flex justify-center gap-8 items-start py-8 bg-zinc-900 h-full w-screen">
+        <CardAside />
+        <div className="grid grid-cols-1 gap-12 w-full max-w-2xl">
+        {posts.map((post) => (<Post key={post.id} {...post} />))}
+        </div>
+        
+      </div>
     </>
   );
 }
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,14 +1,32 @@
+import { Post as PostType, User} from "@/App";
 import { FormComents } from "./FormComents";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
+import { useEffect, useState } from "react";
 
-export function Post() {
+export function Post(props: PostType) {
+  const [user, setUser] = useState<User>()
+
+  async function getUser() {
+    try {
+      const response = await fetch('http://localhost:3000/users/'+ props.userId)
+      const data = await response.json()
+      setUser(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
   return (
     <>
-      <div className="w-3/6 max-h-screen bg-zinc-800 rounded-xl flex flex-col p-10">
+      <div className=" bg-zinc-800 rounded-xl flex flex-col p-10">
         <div className="flex">
           <img
-            src="darth.jpg"
+            src={user?.profileUrl}
             alt="darth vader serio"
             className="w-16 h-16 border-2 border-sky-500 rounded-xl"
           />
@@ -19,9 +37,11 @@ export function Post() {
         </div>
         <div className=" flex flex-col py-5 gap-2">
           <p className="text-white">
-            Às vezes, é necessário usar o lado negro da força!
+           {props.content}
           </p>
-          <p className="text-sky-500">#DarkSide #SithLife</p>
+          <p className="text-sky-500">
+            {props.hashtags.join(', ')}
+          </p>
         </div>
 
         <Separator className="bg-zinc-600 my-4" />
